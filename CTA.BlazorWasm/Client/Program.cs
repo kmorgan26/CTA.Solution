116c06ec08cc1bde3a428fe0969be3aad896d582
Kevin Morgan <kmorgan26@gmail.com>
2022-02-19 08:26:12 -0600

    Modify program.cs for Blazorise

--- CTA.BlazorWasm/Client/Program.cs
+++ CTA.BlazorWasm/Client/Program.cs
@@ -2,10 +2,34 @@ using CTA.BlazorWasm.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+// other usings
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-await builder.Build().RunAsync();
+        builder.Services
+          .AddBlazorise(options =>
+          {
+              options.ChangeTextOnKeyPress = true;
+          })
+          .AddBootstrapProviders()
+          .AddFontAwesomeIcons();
+
+        builder.Services.AddSingleton(new HttpClient
+        {
+            BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+        });
+
+        builder.RootComponents.Add<App>("#app");
+
+        var host = builder.Build();
+
+        await host.RunAsync();
+    }
+}
