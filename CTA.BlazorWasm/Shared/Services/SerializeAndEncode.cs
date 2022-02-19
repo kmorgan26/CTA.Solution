80dae31dba0513fbcb29fddb97f3d5b53b4bc463
Kevin Morgan <kmorgan26@gmail.com>
2022-02-19 08:57:17 -0600

    Add Services to Shared Project

---
+++ CTA.BlazorWasm/Shared/Services/SerializeAndEncode.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace CTA.BlazorWasm.Shared.Services
+{
+    public static class SerializeAndEncode
+    {
+        public static async Task<string> ObjectToJsonAndEncode(object objectToSerialize)
+        {
+            var json = await Task.Run(() => System.Text.Json.JsonSerializer.Serialize(objectToSerialize));
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(json));
+        }
+
+        public static async Task<string> EncodedStringToJson(string encodedString)
+        {
+            byte[] byteArray = await Task.Run(() => Convert.FromBase64String(encodedString));
+            return Encoding.UTF8.GetString(byteArray);
+        }
+    }
+}
