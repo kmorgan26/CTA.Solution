c3afb974557d856200df33c0b611b0320f838031
Kevin Morgan <kmorgan26@gmail.com>
2022-03-03 07:17:39 -0600

    arrange properties in tracking filter

--- CTA.BlazorWasm/Shared/Filters/TrackingFilter.cs
+++ CTA.BlazorWasm/Shared/Filters/TrackingFilter.cs
@@ -8,21 +8,18 @@ namespace CTA.BlazorWasm.Shared.Filters
 {
     public class TrackingFilter
     {
+        public int? ToFromId { get; set; }
+        public int? StatusId { get; set; }
+        public int? CorrTypeId { get; set; }
+        public int? PocId { get; set; }
+        public int ThreadId { get; set; } = 0;
         public string? SubjectText { get; set; }
         public string? CommentsText { get; set; }
-        public int ThreadId { get; set; } = 0;
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public int[]? ToFromIds { get; set; }
         public int[]? TypeIds { get; set; }
         public int[]? PocIds { get; set; }
         public int[]? StatusIds { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-
-        public int? ToFromId { get; set; }
-        public int? StatusId { get; set; }
-        public int? CorrTypeId { get; set; }
-
-        public int? PocId { get; set; }
-
     }
 }
